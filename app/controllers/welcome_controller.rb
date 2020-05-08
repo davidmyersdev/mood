bd83eb21548a50b4106003148000f5bbc493fc71
David R. Myers <davidrmyersii@gmail.com>
2020-05-08 17:55:09 -0400

    Add dashboard and refactor js
    In firefox, asking for permission to send notifications has to be triggered by a user action. It looks like this will soon be the case for other browsers too.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_by_session
 
   def index
-    redirect_to history_index_path if current_user.persisted?
+    redirect_to dashboard_index_path if current_user.persisted?
   end
 
   def sign_up
