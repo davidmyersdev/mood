157b7fac815533a72f86e7bf73d6f1cee3a1727c
David R. Myers <davidrmyersii@gmail.com>
2020-05-17 13:04:44 -0400

    Use Sentry for frontend

--- app/helpers/env_helper.rb
+++ app/helpers/env_helper.rb
@@ -1,4 +1,12 @@
-module CredentialsHelper
+module EnvHelper
+  def app_env
+    Rails.env.presence
+  end
+
+  def sentry_client_dsn
+    Env.sentry_client_dsn
+  end
+
   def vapid_public_key
     Env.vapid_public_key
   end
--- lib/env.rb
+++ lib/env.rb
@@ -5,19 +5,31 @@ class Env
     end
 
     def secret_key_base
-      ENV['SECRET_KEY_BASE'].presence || Rails.application.credentials.secret_key_base
+      ENV['SECRET_KEY_BASE'].presence || Rails.application.credentials.secret_key_base.presence
+    end
+
+    def sentry_client_dsn
+      ENV['SENTRY_CLIENT_DSN'].presence
+    end
+
+    def sentry_server_dsn
+      ENV['SENTRY_DSN'].presence
+    end
+
+    def time_zone
+      ENV['RAILS_TIME_ZONE'].presence || Rails.configuration.time_zone.presence
     end
 
     def vapid_public_key
-      ENV['VAPID_PUBLIC_KEY'].presence || Rails.application.credentials.vapid_public_key
+      ENV['VAPID_PUBLIC_KEY'].presence || Rails.application.credentials.vapid_public_key.presence
     end
 
     def vapid_private_key
-      ENV['VAPID_PRIVATE_KEY'].presence || Rails.application.credentials.vapid_private_key
+      ENV['VAPID_PRIVATE_KEY'].presence || Rails.application.credentials.vapid_private_key.presence
     end
 
     def vapid_subject
-      ENV['VAPID_SUBJECT'].presence || Rails.application.credentials.vapid_subject
+      ENV['VAPID_SUBJECT'].presence || Rails.application.credentials.vapid_subject.presence
     end
   end
 end
